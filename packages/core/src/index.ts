48d194266a39a9b7ea3221dbaf92cd0bcd3bb234
Jiabin <ibin@qq.com>
2023-10-31 13:47:51 +0800

    feat: doc文档编译处理

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,9 +3,18 @@ export * from './is'
 export * from './object'
 export * from './string'
 
-export const testFunc  = () => {
-  console.log(`{
-	"presets": ["@babel/env"]
-}: `, );
+/**
+ * This comment _supports_ [Markdown](https://marked.js.org/)
+ */
+export class DocumentMe {}
 
+/**
+ * aaaaaaaaaaaaa
+ * 
+ * @param value - 要获取类型的值。
+ * @returns 值的类型字符串。
+ */
+export function clampNumberWithinRangsnfoiee(numberToClamp: number, range: [number, number]): number {
+  const [min, max] = range;
+  return Math.max(min, Math.min(numberToClamp, max));
 }
