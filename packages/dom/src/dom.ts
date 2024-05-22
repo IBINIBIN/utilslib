1b3d6cafb1961d387e2f4bd2df98f8cff083620c
IBIN <ibin@qq.com>
2024-05-22 15:11:08 +0800

    fix: 方法重构

--- packages/dom/src/dom.ts
+++ packages/dom/src/dom.ts
@@ -3,7 +3,7 @@
  */
 import raf from "raf";
 import { easeInOutCubic, EasingFunction } from "./easing";
-import { isFunction, isString, toArray } from "@utilslib/core";
+import { isFunction, isString, toArray, NOOP } from "@utilslib/core";
 
 type CSSSelector = string;
 
@@ -17,6 +17,130 @@ type ScrollContainer = (() => ScrollContainerElement) | CSSSelector;
  */
 export const isServer = typeof window === "undefined";
 
+// #region ====================== 已重构的方法 ======================
+
+// 辅助类型，根据传入的节点类型 E 获取对应的事件类型映射
+type EEventMap<E extends Node> = E extends HTMLElement
+  ? HTMLElementEventMap
+  : E extends Document
+  ? DocumentEventMap
+  : E extends Window
+  ? WindowEventMap
+  : GlobalEventHandlersEventMap;
+
+// 辅助类型，根据传入的节点类型 E 获取对应的事件处理函数类型
+type EEventHandler<E extends Node, K extends keyof EEventMap<E>> = (
+  this: E,
+  ev: EEventMap<E>[K]
+) => any;
+
+/**
+ * 给指定元素添加事件监听器，并返回一个函数用于移除监听器。
+ *
+ * @type {<E extends Node, K extends keyof EEventMap<E>>(
+ *   element: E,
+ *   event: K,
+ *   handler: EEventHandler<E, K>,
+ *   options?: boolean | AddEventListenerOptions
+ * ) => typeof NOOP}
+ * @param {E} element - 要添加事件监听器的元素。
+ * @param {K} event - 要监听的事件类型。
+ * @param {EEventHandler<E, K>} handler - 事件处理函数。
+ * @param {boolean | AddEventListenerOptions} [options] - 可选的事件参数。
+ * @returns {typeof NOOP} 一个函数，用于移除事件监听器。
+ */
+export function on<E extends Node, K extends keyof EEventMap<E>>(
+  element: E,
+  event: K,
+  handler: EEventHandler<E, K>,
+  options?: boolean | AddEventListenerOptions
+): typeof NOOP {
+  if (element && event && handler) {
+    element.addEventListener(
+      event as string,
+      handler as EventListenerOrEventListenerObject,
+      options
+    );
+    return () => {
+      element.removeEventListener(
+        event as string,
+        handler as EventListenerOrEventListenerObject,
+        options
+      );
+    };
+  }
+  return NOOP;
+}
+
+/**
+ * 为指定节点的指定事件绑定一个只执行一次的事件处理函数。
+ * @type {<E extends Node, K extends keyof EEventMap<E>>(
+ *   element: E,
+ *   event: K,
+ *   handler: EEventHandler<E, K>,
+ *   options?: boolean | AddEventListenerOptions
+ * ) => typeof NOOP}
+ * @param {E} element - 要添加事件监听器的元素。
+ * @param {K} event - 要监听的事件类型。
+ * @param {EEventHandler<E, K>} handler - 事件处理函数。
+ * @param {boolean | AddEventListenerOptions} [options] - 可选的事件参数。
+ * @returns {typeof NOOP} 一个函数，用于移除事件监听器。
+ */
+export function once<E extends Node, K extends keyof EEventMap<E>>(
+  element: E,
+  event: K,
+  handler: EEventHandler<E, K>,
+  options?: boolean | AddEventListenerOptions
+): typeof NOOP {
+  const callback: EEventHandler<E, K> = function (e) {
+    handler.call(this, e);
+    off(element, event, callback, options);
+  };
+
+  on(element, event, callback, options);
+
+  return () => {
+    off(element, event, callback, options);
+  };
+}
+
+/**
+ * 获取指定节点对应的 HTMLElement 或 Element。
+ *
+ * @param {T} node - 可以是一个字符串选择器、一个 HTMLElement、或返回 HTMLElement 的函数。
+ * @returns {HTMLElement | Element | undefined} 返回找到的 HTMLElement 或 Element，找不到则返回 undefined。
+ */
+export function getElement(
+  node: Element | HTMLElement | Function | string
+): HTMLElement | Element | undefined {
+  const attachNode = node instanceof Function ? node() : node;
+  if (!attachNode) {
+    return undefined;
+  }
+  if (typeof attachNode === "string") {
+    return document.querySelector(attachNode)!;
+  }
+  if (attachNode instanceof Element) {
+    return attachNode;
+  }
+  return undefined;
+}
+
+/**
+ * 获取要附加到的节点元素。
+ * @param {T} node - 可以是一个字符串选择器、一个 HTMLElement、或返回 HTMLElement 的函数。
+ * @returns {HTMLElement | Element } 返回找到的 HTMLElement 或 Element，找不到则返回 undefined。
+ */
+export const getAttach: (
+  ...args: Parameters<typeof getElement>
+) => NonNullable<ReturnType<typeof getElement>> = function (
+  node
+): NonNullable<ReturnType<typeof getElement>> {
+  return getElement(node) ?? document.body;
+};
+
+// #endregion -- end
+
 /**
  * 使用正则表达式去除字符串两端的空格和特殊空白字符。
  * @param {string} str - 需要去除空格的字符串
@@ -25,40 +149,13 @@ export const isServer = typeof window === "undefined";
 const trim = (str: string): string => (str || "").replace(/^[\s\uFEFF]+|[\s\uFEFF]+$/g, "");
 
 /**
- * 动态返回一个事件绑定函数，根据运行环境选择使用addEventListener或attachEvent。
- * @param {Node} element - 要绑定事件的节点
- * @param {string} event - 事件类型
- * @param {EventListenerOrEventListenerObject} handler - 事件处理函数
- * @param {boolean | AddEventListenerOptions} [options] - 可选的事件参数
- * @returns {any} 返回一个函数，用于绑定指定事件到指定节点
- */
-export const on = ((): any => {
-  if (!isServer && Boolean(document.addEventListener)) {
-    return (
-      element: Node,
-      event: string,
-      handler: EventListenerOrEventListenerObject,
-      options?: boolean | AddEventListenerOptions
-    ): any => {
-      if (element && event && handler) {
-        element.addEventListener(event, handler, options);
-      }
-    };
-  }
-  return (element: Node, event: string, handler: EventListenerOrEventListenerObject): any => {
-    if (element && event && handler) {
-      (element as any).attachEvent(`on${event}`, handler);
-    }
-  };
-})();
-
-/**
  * 动态返回一个事件解绑函数，根据运行环境选择使用removeEventListener或detachEvent。
  * @param {Node} element - 要解绑事件的节点
  * @param {string} event - 事件类型
  * @param {EventListenerOrEventListenerObject} handler - 事件处理函数
  * @param {boolean | AddEventListenerOptions} [options] - 可选的事件参数
  * @returns {any} 返回一个函数，用于解绑指定事件从指定节点
+ * @alpha
  */
 export const off = ((): any => {
   if (!isServer && Boolean(document.removeEventListener)) {
@@ -81,28 +178,6 @@ export const off = ((): any => {
 })();
 
 /**
- * 为指定节点的指定事件绑定一个只执行一次的事件处理函数。
- * @param {Node} element - 要绑定事件的节点
- * @param {string} event - 事件类型
- * @param {EventListenerOrEventListenerObject} handler - 事件处理函数
- * @param {boolean | AddEventListenerOptions} [options] - 可选的事件参数
- */
-export function once(
-  element: Node,
-  event: string,
-  handler: EventListenerOrEventListenerObject,
-  options?: boolean | AddEventListenerOptions
-) {
-  const handlerFn = isFunction(handler) ? handler : handler.handleEvent;
-  const callback = (evt: any) => {
-    handlerFn(evt);
-    off(element, event, callback, options);
-  };
-
-  on(element, event, callback, options);
-}
-
-/**
  * 创建一个用于管理事件监听器的工具函数
  * @param elm - 要添加事件监听器的元素引用
  * @returns 返回一个包含添加和清除事件监听器功能的对象
@@ -110,9 +185,9 @@ export function once(
 export function attachListeners(elm: Element) {
   const offs: Array<() => void> = [];
   return {
-    add<K extends keyof HTMLElementEventMap>(
+    add<K extends keyof GlobalEventHandlersEventMap>(
       type: K,
-      listener: (ev: HTMLElementEventMap[K]) => void
+      listener: (ev: GlobalEventHandlersEventMap[K]) => void
     ) {
       if (!type) return;
       on(elm, type, listener);
@@ -191,26 +266,6 @@ export function removeClass(el: Element, cls: string): any {
     el.className = trim(curClass);
   }
 }
-
-/**
- * 获取要附加到的节点元素。
- * @param {any} node - 要附加到的节点元素，可以是函数、选择器字符串或元素对象
- * @param {any} [triggerNode] - 触发节点元素，可选
- * @returns {HTMLElement | Element} 返回要附加到的节点元素
- */
-export const getAttach = (node: any, triggerNode?: any): HTMLElement | Element => {
-  const attachNode = isFunction(node) ? node(triggerNode) : node;
-  if (!attachNode) {
-    return document.body;
-  }
-  if (isString(attachNode)) {
-    return document.querySelector(attachNode)!;
-  }
-  if (attachNode instanceof HTMLElement) {
-    return attachNode;
-  }
-  return document.body;
-};
 
 /**
  * 获取滚动容器元素。
@@ -305,19 +360,18 @@ export function scrollTo(target: number, opt: ScrollTopOptions): Promise<ScrollT
 
 /**
  * 检查父元素是否包含子元素。
- * @param {Element | Iterable<any> | ArrayLike<any>} parent - 父元素
+ * @param {Node} parent - 父元素
  * @param {any} child - 子元素
  * @returns {boolean} 如果父元素包含子元素则返回true，否则返回false
  */
-function containerDom(parent: Element | Iterable<any> | ArrayLike<any>, child: any): boolean {
+export function containerDom(parent: Node, child: any): boolean {
   if (parent && child) {
     let pNode = child;
     while (pNode) {
       if (parent === pNode) {
         return true;
       }
-      const { parentNode } = pNode;
-      pNode = parentNode;
+      pNode = pNode.parentNode;
     }
   }
   return false;
@@ -327,27 +381,47 @@ function containerDom(parent: Element | Iterable<any> | ArrayLike<any>, child: a
  * 监听点击事件，当点击元素在指定元素之外时执行回调函数。
  * @param {Element | Iterable<any> | ArrayLike<any>} els - 指定的元素或元素集合
  * @param {() => void} cb - 点击元素在指定元素之外时执行的回调函数
+ * @returns {() => void} 一个函数，用于销毁监听事件。
  */
-export const clickOut = (els: Element | Iterable<any> | ArrayLike<any>, cb: () => void): void => {
-  on(document, "click", (event: { target: Element }) => {
-    els = toArray(els);
-    const isFlag = Array.from(els).every((item) => containerDom(item, event.target) === false);
+
+export function clickOut(els: Element | Element[], cb: () => void) {
+  return on(document, "click", (event) => {
+    const _els = toArray(els);
+    const isFlag = _els.every((item) => !containerDom(item, event.target));
     return isFlag && cb && cb();
   });
-};
+}
 
-/**
- * 检查节点是否发生溢出。
- * @param {Element |  Element[]} ele - 要检查的节点或节点数组
- * @returns {boolean} 如果节点发生溢出则返回true，否则返回false
- */
-export const isNodeOverflow = (ele: Element | Element[]): boolean => {
-  const { clientWidth = 0, scrollWidth = 0 } = ele as Element & {
-    clientWidth: number;
-    scrollWidth: number;
-  };
-  return scrollWidth > clientWidth;
-};
+export function clickOutInWebComponent({
+  els,
+  cb,
+  root,
+}: {
+  els: Element | Iterable<any> | ArrayLike<any> | Function;
+  cb: () => void;
+  root: Node;
+  [key: string]: any;
+}): typeof NOOP {
+  // let doc, root;
+  on(root, "click", (event) => {
+    const _els = toArray(els).filter(Boolean);
+    const protect = _els.some((item) => containerDom(item?.() ?? item, event.target));
+    return !protect && cb();
+  });
+
+  if (root !== document) {
+    on(document, "click", (e) => {
+      if (!(e.target instanceof HTMLElement)) return;
+
+      const clickRoot = e.target.shadowRoot;
+      if (clickRoot !== root) {
+        cb();
+      }
+    });
+  }
+
+  return () => {};
+}
 
 /**
  * 将选定的元素滚动到父元素的可视区域内。
