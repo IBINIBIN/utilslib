a66d26c5ca8a4e8f1f12b00c545e456a712a2fd0
IBIN <ibin@qq.com>
2024-05-29 11:04:28 +0800

    feat: 新增常用时间变量

---
+++ packages/core/src/constants.ts
@@ -0,0 +1,9 @@
+export const ONE_SECOND_IN_MS = 1000;
+export const ONE_MINUTE_IN_MS = 60 * 1000;
+export const ONE_HOUR_IN_MS = 60 * 60 * 1000;
+export const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
+
+export const ONE_SECOND_IN_S = 1;
+export const ONE_MINUTE_IN_S = 60;
+export const ONE_HOUR_IN_S = 60 * 60;
+export const ONE_DAY_IN_S = 24 * 60 * 60;
