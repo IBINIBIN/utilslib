0572d1a4ca9d92155ed7f40b2d1cdf2e57326b7e
Jiabin <ibin@qq.com>
2024-11-29 12:34:21 +0800

    feat: 新增vitepress文档

--- config/typedoc.config.js
+++ config/typedoc.config.js
@@ -3,12 +3,12 @@ import { fileURLToPath } from "url";
 
 /** @type {import('typedoc').TypeDocOptions} */
 export default {
-  name: "@utilslib",
+  name: "UtilsLib",
+  out: '../docs/typedoc',
   readme: fileURLToPath(new URL("../README.md", import.meta.url)),
   entryPoints: globSync("packages/*").map((item) => "../" + item),
   entryPointStrategy: "packages",
   hideGenerator: true,
-  // plugin: ["typedoc-plugin-markdown"],
   includeVersion: false,
   navigationLinks: {
     GitHub: "https://github.com/T-Tuan/utilslib",
